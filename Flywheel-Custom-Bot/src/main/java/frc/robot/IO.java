1e9f7dfac4f2d21f0703aeb420de01c3ff40e007
amesrobotics <ames.amperes@gmail.com>
2019-11-23 12:29:13 -0700

    Start MotorController class

--- Flywheel-Custom-Bot/src/main/java/frc/robot/IO.java
+++ Flywheel-Custom-Bot/src/main/java/frc/robot/IO.java
@@ -1,5 +1,27 @@
 package frc.robot;
 
-public class IO{
-	
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class IO {
+	Joystick firstInput = new Joystick(0);
+    Joystick secondInput = new Joystick(1);
+}
+Double [] drivingJoysticks() {
+	Double [] axisVar = new Double[2];
+	axisVar[0] = firstInput.getRawAxis(1);
+	axisVar[1] = firstInput.getRawAxis(3);
+	axisVar = scaleFactor(axisVar);
+	axisVar = deadZone(axisVar);    
+	return axisVar;
+}
+
+Double[] deadZone(Double[] in) {
+	if (-.09 < in[0] && in[0] < .09) {
+		in[0] = 0.0;
+	}
+	if (-.09 < in[1] && in[1] < .09) {
+		in[1] = 0.0;
+	}
+	return in;
 }
