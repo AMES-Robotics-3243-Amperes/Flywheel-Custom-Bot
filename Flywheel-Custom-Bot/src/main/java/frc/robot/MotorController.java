a78a6731f74000591a01a28f414ff4c0b9465bee
LeagueOfFreeWorlds <inathan2347@gmail.com>
2019-12-02 13:10:02 -0700

    Beginning to write code for MotorController class: Constructors and instances

--- Flywheel-Custom-Bot/src/main/java/frc/robot/MotorController.java
+++ Flywheel-Custom-Bot/src/main/java/frc/robot/MotorController.java
@@ -7,13 +7,20 @@ import com.revrobotics.ControlType;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.SpeedController;
+
 // Phoenix Libraries
 //import com.ctre.phoenix.ILoopable;
 //import com.ctre.phoenix.motorcontrol.ControlMode;
 //import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 //import com.ctre.phoenix.motorcontrol.can.*;
 
+// 12/2/19 - Constructor is undefined. Having problems with object
 public class MotorController {
-    private CANSparkMax motor  = new CANSparkMax(1); // some motor stuff
+    public static final int leadDeviceID = 1; // static: accesible outside of the obj / final: value cannot be overriden
+    CANEncoder m_eEncoder; // Not a clue on what this means
+    private CANPIDController leadPIDController; // Calling PID controller
+    private CANSparkMax motor  = new CANSparkMax(leadDeviceID, MotorType.kBrushless); // motor object 
+    public double maxRPM;
+
 	
 }
